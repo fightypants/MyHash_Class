34d2e7df9f231f05c2f90e533500925c71ccd057
Guest User <student@epicodus.com>
2015-04-26 17:50:09 -0700

    It creates the MyHash class

---
+++ lib/MyHash.rb
@@ -0,0 +1,16 @@
+
+
+class MyHash
+  define_method(:intialize) do |key, value|
+    @key = key
+    @value = value
+  end
+
+  define_method(:store) do
+    @key = @vaule
+
+  end
+#
+#   define_method(:fetch) do
+#
+# end
